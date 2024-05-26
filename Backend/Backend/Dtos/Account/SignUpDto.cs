169a71378cd0a4925c0afdfc38f67b049ef25dea
abdullahmokhtar <abdullahmokhtr55@gmail.com>
2024-05-27 01:40:28 +0300

    update validation on backend when signup to return 2 diffrent messages

--- Backend/Backend/Dtos/Account/SignUpDto.cs
+++ Backend/Backend/Dtos/Account/SignUpDto.cs
@@ -2,10 +2,12 @@
 {
     public class SignUpDto
     {
+        [StringLength(255)]
         public string Name { get; set; } = string.Empty;
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-        [RegularExpression("^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$", ErrorMessage = "Password must contain at least 1 uppercase, 1 lowercase, 1 special character, 1 digit and length 8 characters")]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password length between 8 to 16 characters")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$", ErrorMessage = "Password must contain at least 1 uppercase, 1 lowercase, 1 special character, 1 digit and length 8 characters")]
         public string Password { get; set; } = string.Empty;
         [Compare("Password")]
         public string RePassword { get; set; } = string.Empty;
